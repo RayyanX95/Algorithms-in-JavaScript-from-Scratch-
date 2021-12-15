a1c4d583a3d0c281557a7ff817d34601c5f1e0e9
Ibrahim AlRayyan <ibrahim.alrayany@gmail.com>
2021-12-15 10:45:25 +0200

    add binary search algorithm

---
+++ binary-search/index.js
@@ -0,0 +1,16 @@
+const binarySearch = (numArray, key) => {
+  console.log(numArray)
+  const middleIndex = Math.floor(numArray.length / 2);
+  const middleElem = numArray[middleIndex];
+
+  //* Base Case
+  if (middleElem === key) return true;
+  //* Recursive Cases
+  else if (middleElem < key && numArray.length > 1) {
+    return binarySearch(numArray.splice(middleIndex, numArray.length), key);
+  } else if (middleElem > key && numArray.length > 1) {
+    return binarySearch(numArray.splice(0, middleIndex), key);
+  } else return false;
+}
+
+console.log(binarySearch([1, 55, 77, 8, 6, 44, 11, 22, 63, 74].sort((a, b) => a - b), 22))
